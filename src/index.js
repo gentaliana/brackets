899ccdf17c23072de8983e8272e4e5660a807c9e
gentaliana <gentaliana@gmail.com>
2020-10-09 17:09:17 +0400

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const bracketsLength = bracketsConfig.length;
+    for (let i = 0; i < bracketsLength; i++) {
+        let brackets = bracketsConfig[i].join("");
+        while (true) {
+            if (str.includes(brackets)) {
+                str = str.replace(brackets, "");
+                i = -1;
+            } else {
+                break;
+            }
+        }
+    }
+    return str ? false : true;
+};
